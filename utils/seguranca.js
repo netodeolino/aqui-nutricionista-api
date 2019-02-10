4b755f7ea2518d3d37ee90b5a6acac8ae4e15f86
Neto Deolino <netodeolino@gmail.com>
2019-02-09 22:41:28 -0300

    adicionado try catch

--- utils/seguranca.js
+++ utils/seguranca.js
@@ -4,24 +4,28 @@ const jwt = require('jsonwebtoken')
 const { TOKEN_OBRIGATORIO, TOKEN_INVALIDO, ERRO_VERIFICAR_TOKEN } = require('./constants')
 
 function isTokenValidoMiddleware (req, res, next) {
-  const token = req.headers['aqui-nutricionista-token']
-  if (!token) {
-    res.status(500).send(TOKEN_OBRIGATORIO)
-  }
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
-    if (err) {
-      res.status(500).send(TOKEN_INVALIDO)
+  try {
+    const token = req.headers['aqui-nutricionista-token']
+    if (!token) {
+      res.status(500).send(TOKEN_OBRIGATORIO)
     }
-    req.emailUsuarioLogado = decoded.email
-    next()
-  })
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) {
+        res.status(500).send(TOKEN_INVALIDO)
+      }
+      req.emailUsuarioLogado = decoded.email
+      next()
+    })
+  } catch (error) {
+    throw TOKEN_INVALIDO
+  }
 }
 
 function isTokenValido (token) {
-  if (!token) {
-    return false
-  }
   try {
+    if (!token) {
+      return false
+    }
     return jwt.verify(token, process.env.SECRET)
   } catch (error) {
     throw ERRO_VERIFICAR_TOKEN
